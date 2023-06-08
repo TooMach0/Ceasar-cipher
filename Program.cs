a4df9fe4093a595550762cfa8a36d6355ec0a9c0
Toomach0 <michal.cunta@protonmail.com>
2023-06-08 12:34:37 +0200

    pridal som writeline

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace ConsoleApp
             char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                                            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
+            Console.WriteLine(alphabet);
 
             do
             {
